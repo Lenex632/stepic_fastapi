b16da6faff04e458a9e7f59c62fc485d065864f0
Lenex <ivankrasilnikov632@gmail.com>
2026-02-20 22:05:26 +0300

    Начало работы с базой данных. Создание таблиц.

---
+++ app/database.py
@@ -0,0 +1,59 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "sqlite:///test_project.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+# ----------------------------------------------------------------------
+# from sqlalchemy import Column, Integer, String, Float, Table
+# from sqlalchemy.orm import registry, mapped_column, Mapped
+# from datetime import datetime
+#
+#
+# ----------------------------------------------------------------------
+# class Product(Base):
+#     __tablename__ = "products"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+#     sku: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+#     name: Mapped[str] = mapped_column(String(100), nullable=False, index=True)
+#     description: Mapped[str] = mapped_column(String(500), default="")
+#     price: Mapped[float] = mapped_column(nullable=False)
+#     is_available: Mapped[bool] = mapped_column(default=False)
+#     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+#
+#
+# ----------------------------------------------------------------------
+# class Product(Base):
+#     __tablename__ = "products"
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String, nullable=False)
+#     description = Column(String)
+#     price = Column(Float, nullable=False)
+#
+#
+# ----------------------------------------------------------------------
+# mapper_registry = registry()
+# metadata = mapper_registry.metadata
+#
+#
+# product_table = Table(
+#     "products",
+#     metadata,
+#     Column("id", Integer, primary_key=True, index=True),
+#     Column("name", String, nullable=False),
+#     Column("description", String),
+#     Column("price", Float, nullable=False)
+# )
+#
+# metadata.create_all(engine)
+# ----------------------------------------------------------------------
---
+++ app/models/categories.py
@@ -0,0 +1,105 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+
+# --------------------------------------------------------------------------------
+# from decimal import Decimal
+# from datetime import date, datetime
+#
+# from sqlalchemy import String, Boolean, Numeric, Date, DateTime, Text
+# from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+#
+#
+# class Base(DeclarativeBase):
+#     pass
+#
+#
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# class Recipe(Base):
+#     __tablename__ = "recipes"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(150), nullable=False)
+#     cooking_time: Mapped[int] = mapped_column(nullable=False)
+#     ingredients: Mapped[str] = mapped_column(Text, nullable=False)
+#     calories: Mapped[int]
+#     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now)
+#
+#
+# --------------------------------------------------------------------------------
+# class Car(Base):
+#     __tablename__ = "cars"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     brand: Mapped[str] = mapped_column(String(50), nullable=False)
+#     model: Mapped[str] = mapped_column(String(50), nullable=False)
+#     year: Mapped[int] = mapped_column(nullable=False)
+#     price: Mapped[Decimal | None] = mapped_column(Numeric(10, 2))
+#     is_available: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+#
+#
+# # --------------------------------------------------------------------------------
+# class Event(Base):
+#     __tablename__ = "events"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     title: Mapped[str] = mapped_column(String(100), nullable=False)
+#     start_time: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
+#     end_time: Mapped[datetime | None] = mapped_column(DateTime())
+#     description: Mapped[str | None] = mapped_column(Text())
+#
+#
+# --------------------------------------------------------------------------------
+# class Movie(Base):
+#     __tablename__ = "movies"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     title: Mapped[str] = mapped_column(String(200), nullable=False)
+#     duration: Mapped[int] = mapped_column(nullable=False)
+#     release_date: Mapped[date | None] = mapped_column(Date(), nullable=True)
+#
+#
+# --------------------------------------------------------------------------------
+# class Product(Base):
+#     __tablename__ = "products"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(150), nullable=False)
+#     price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+#     stock_quantity: Mapped[int] = mapped_column(nullable=False, default=0)
+#
+#
+# --------------------------------------------------------------------------------
+# class Task(Base):
+#     __tablename__ = "tasks"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     title: Mapped[str] = mapped_column(String(100), nullable=False)
+#     is_completed: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+#
+#
+# --------------------------------------------------------------------------------
+# class Category(Base):
+#     __tablename__ = "categories"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(50), nullable=False)
+# --------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    from sqlalchemy.schema import CreateTable
+    from app.models.products import Product
+    print(CreateTable(Category.__table__))
+    print(CreateTable(Product.__table__))
+
