f8628e515d684b516fb0fe186a20a9260b38af56
Lenex <ivankrasilnikov632@gmail.com>
2026-02-20 10:56:47 +0300

    Начало работы с тестовым проектом Магазина

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
+
+from app.routers import categories, products
+
+
+app = FastAPI(
+    title="Тестовый интернет магазин",
+    version="0.1.0"
+)
+
+app.include_router(categories.router)
+app.include_router(products.router)
+
+
+@app.get("/")
+async def root():
+    """
+    Корневой маршрут, подтверждающий, что API работает.
+    """
+    return {"message": "Добро пожаловать в API интернет-магазина!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
