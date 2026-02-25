3a0ae86a8f269a6709a1e3ff8f7a55fb5266c8ca
Lenex <ivankrasilnikov632@gmail.com>
2026-02-25 17:29:20 +0300

    Изменение работы эндпойнтов категорий на асинхронные

--- app/db_depends.py
+++ app/db_depends.py
@@ -15,3 +15,16 @@ def get_db() -> Generator[Session, None, None]:
     finally:
         db.close()
 
+
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import async_session_maker
+
+
+async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Представляет асинхронную сессию SQLalchemy для работы с базой данных PostgresSQL.
+    """
+    async with async_session_maker() as session:
+        yield session
+
