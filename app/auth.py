476c2180aa9877b0ec7eb562c193779188ca631a
lenex <ivankrasilnikov632@gmail.com>
2026-03-01 16:06:10 +0300

    Добоавление работы с токенами. Добоавление работы с .env.

--- app/auth.py
+++ app/auth.py
@@ -1,8 +1,23 @@
+import jwt
+
+from datetime import datetime, timedelta, timezone
+
 from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.users import User as UserModel
+from app.config import SECRET_KEY, ALGORITHM
+from app.db_depends import get_async_db
 
 
 # Создаём контекст для хеширования с использованием bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="users/token")
 
 
 def hash_password(password: str) -> str:
@@ -17,4 +32,46 @@ def verify_password(plain_password: str, hash_password: str) -> bool:
     Проверяет, соответствует ли введённый пароль сохранённому хешу.
     """
     return pwd_context.verify(plain_password, hash_password)
+
+
+def create_access_token(data: dict):
+    """
+    Создаёт JWT с payload (sub, role, id, exp).
+    """
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: str = Depends(oauth2_schema), db: AsyncSession = Depends(get_async_db)):
+    """
+    Проверяет JWT и возвращает пользователя из базы.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    result = await db.scalars(
+        select(UserModel).where(UserModel.email == email, UserModel.is_active.is_(True)))
+    user = result.first()
+    if user is None:
+        raise credentials_exception
+
+    return user
 
